d44f263c3682ef72f989b691a72155d645296b5f
Matt Halpern <mhalpern3719@gmail.com>
2012-04-04 23:56:25 -0500

    Add the other half of the jank commit, let's put this behind us, ok :P

---
+++ src/samuraijam/spriteParts/Bridge.py
@@ -0,0 +1,27 @@
+'''
+Created on Apr 4, 2012
+
+@author: Matt Halpern
+'''
+
+import pygame
+
+class Bridge(object):
+    
+
+
+    def __init__(self, xPos, yPos):
+        
+        pygame.sprite.Sprite.__init__(self) 
+        #self.image, self.rect = load_image('pinkcreep.png',-1)
+        
+        self.image = pygame.image.load('../../../data/bluecreep.png')
+        self.rect = self.image.get_rect()
+        
+        self.newX = width-self.rect.width
+        self.rect.topleft = (self.newX,yPos)
+        
+    def update(self, dist):
+        #self.newX = self.newX - 1
+        #self.rect.topleft = (self.newX,self.rect.y)
+        self.rect.move_ip(-dist,0)
---
+++ src/samuraijam/testing/WaitTest.py
@@ -0,0 +1,7 @@
+'''
+Created on Apr 4, 2012
+
+@author: Matt Halpern
+'''
+import time
+
