b1987d579b63ab27b0f2ccb3f4e30ea2c2cf2cb3
Dave Lee <camperdave614@gmail.com>
2012-04-19 17:00:38 -0500

    Enemy Spawning now works! Previously, we only had mines....

--- src/samuraijam/enemies/Enemy.py
+++ src/samuraijam/enemies/Enemy.py
@@ -1,19 +1,18 @@
 import os, sys
 import pygame
-#from Helpers import *
+from samuraijam.util.Helpers import *
 from pygame.locals import *
 
 class Enemy(pygame.sprite.Sprite):
 
-    def __init__(self, velocity, width, yPos):
+    def __init__(self, xPos, yPos):
         pygame.sprite.Sprite.__init__(self) 
         #self.image, self.rect = load_image('pinkcreep.png',-1)
         
-        self.image = pygame.image.load('../data/pinkcreep.png')
-        self.rect = self.image.get_rect()
+        self.image, self.rect = load_image('pinkcreep.png',-1)
+        #self.rect = self.image.get_rect()
         
-        self.newX = width-self.rect.width
-        self.rect.topleft = (self.newX,yPos)
+        self.rect.topleft = (xPos,yPos)
         
     def update(self, dist):
         #self.newX = self.newX - 1
