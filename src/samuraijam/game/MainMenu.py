aa033fa8ee71f8d6c0afc477d2d448733ef40b43
Dave <Dave@Dave-PC.resnet.wustl.edu>
2012-04-19 16:40:06 -0500

    win32 now supports moving the menu with the guitar, darwin just needs control handler

--- src/samuraijam/game/MainMenu.py
+++ src/samuraijam/game/MainMenu.py
@@ -19,9 +19,32 @@ class MainMenu:
         pygame.init()
         
         """Joystick"""
-        #self.joy=pygame.joystick.Joystick(0)
-        #self.joy.init()
+        self.joy=pygame.joystick.Joystick(0)
+        self.joy.init()
         #self.numbutton = self.joy.get_numbuttons()
+        
+        if sys.platform == "darwin":
+            self.buttonMap = {11 : HAL.GREEN, 12 : HAL.RED, 13 : HAL.BLUE, 14 : HAL.YELLOW, 8 : HAL.ORANGE, 5 : HAL.BACK, 4 : HAL.START, 1 : HAL.STRUM_DOWN, 0 : HAL.STRUM_UP}
+        
+            self.axisMap = {4 : HAL.WHAMMY, 1 : HAL.EFFECT, 5 : HAL.TILT, 2 : HAL.NOTHING, 3 : HAL.NOTHING}
+            
+            self.hatMap = {0 : {} }
+            
+            self.axisDefault = {4 : -1.0}
+            self.hatDefault = {}
+            
+        elif sys.platform == "win32":
+            self.buttonMap = {0 : HAL.GREEN, 1 : HAL.RED, 2 : HAL.BLUE, 3 : HAL.YELLOW, 4 : HAL.ORANGE, 6 : HAL.BACK, 7 : HAL.START}
+            self.axisMap = {4 : HAL.WHAMMY, 2 : HAL.EFFECT, 3 : HAL.TILT}
+            
+            self.hatMap = {0 : { (0, -1) : HAL.STRUM_DOWN, (0, 1) : HAL.STRUM_UP} }
+            
+            self.axisDefault = {4 : -1.0}
+            self.hatDefault = {}
+            
+        self.hal = HAL(self.buttonMap, self.axisMap, self.hatMap, self.axisDefault, self.hatDefault)
+        
+        
         self.screen = screen
         # Load two sounds
         #self.fluteMusic = pygame.mixer.Sound("../data/introFlute.ogg")
@@ -95,6 +118,23 @@ class MainMenu:
             
             # Get the next event
             e = pygame.event.wait()
+            
+            if sys.platform == "win32":
+                if e.type == pygame.JOYHATMOTION:
+                    print "LOL JOY HAT"
+                    g_state = self.hal.parseAll(self.joy)
+                    if (HAL.STRUM_DOWN in g_state and g_state[HAL.STRUM_DOWN] == True):
+                        print "down"
+                        e = pygame.event.Event(pygame.KEYDOWN, key = pygame.K_DOWN)
+                    if (HAL.STRUM_UP in g_state and g_state[HAL.STRUM_UP] == True):
+                        print "up"
+                        e = pygame.event.Event(pygame.KEYDOWN, key = pygame.K_UP)
+                elif e.type == pygame.JOYBUTTONDOWN:
+                    print "LOL JOY BUTTON"
+                    g_state = self.hal.parseButton(self.joy)
+                    if (HAL.GREEN in g_state) and (g_state[HAL.GREEN] == True):
+                        print "green"
+                        e = pygame.event.Event(pygame.KEYDOWN, key = pygame.K_RETURN)
             
             # Update the menu, based on which "state" we are in - When using the menu
             # in a more complex program, definitely make the states global variables
