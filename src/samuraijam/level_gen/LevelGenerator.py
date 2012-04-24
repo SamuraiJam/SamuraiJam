1a2a8f7f445f975bd90459722c5f9e0d648be0ea
Matt Halpern <mhalpern3719@gmail.com>
2012-04-24 00:45:20 -0500

    levels now have 3 enemies (2=groupie, 3=bodyguard, 4=lawyer) and powerups (5)

--- src/samuraijam/level_gen/LevelGenerator.py
+++ src/samuraijam/level_gen/LevelGenerator.py
@@ -8,83 +8,90 @@ import random
 PROB_MINE = .05
 PROB_ENEMY = .07
 #sum of probs must be 1
-PROB_ENEMY_TYPE = [(2, 0.35), (3, 0.25), (4, 0.2), (5, 0.2)]
+#2=groupie, 3=cop, 4=lawyer
+PROB_ENEMY_TYPE = [(2, 0.6), (3, 0.3), (4, 0.1)]
 PROB_DOWN = .4
 PROB_UP = .4
 PROB_BRIDGE = .05
+PROB_POWERUP = .03
 MOVE_FREQ = 8
 START_STRING = 2
 
-INPUT_FILE = "firework-times.txt"
-OUTPUT_FILE = "firework-level.txt"
+SONGS = ["sexy", "tiktok", "firework"]
 
-input = open("../../../data/" + INPUT_FILE, "r")
-output = open("../../../data/" + OUTPUT_FILE, "w")
-linenumber = 0
-lastindex = START_STRING
-
-for line in input:
-    linenumber = linenumber + 1
-    line = line.rstrip()
-    if linenumber == 1:
-        output.write(str(0.0) + "\t")
-    else:
-        output.write(line + "\t")
-    if linenumber % MOVE_FREQ == 1:
-        r = random.random()
-        if r < PROB_DOWN:
-            move = -1
-        elif r < 1-PROB_UP:
-            move = 0
-        else:
-            move = 1
-        if (lastindex == 0 and move == -1) or (lastindex == 5 and move == 1) or (move == 0):
-            newindex = lastindex
-        else:
-            newindex = lastindex + move     
-    else:
-        newindex = lastindex
-    bridgeline = ""
-    newline = ""
-    minindex = min(lastindex, newindex)
-    maxindex = max(lastindex, newindex)
-    for i in range(0, minindex):
-        r = random.random()
-        if r < PROB_MINE:
-            newline = newline + "1"
-        else:
-            newline = newline + "0"
-        r = random.random()
-        if r < PROB_BRIDGE:
-            bridgeline = bridgeline + "1"
-        else:
-            bridgeline = bridgeline + "0"
-    r = random.random()
-    if newindex == lastindex:
-        if r < PROB_ENEMY:
-            r = random.random()
-            cum = 0
-            for type, prob in PROB_ENEMY_TYPE:
-                if cum <= r and r < cum + prob:
-                    newline = newline + str(type)
-                cum = cum + prob
-        else:
-            newline = newline + "0"
-    else:
-        newline = newline + "00"
-        bridgeline = bridgeline + "1"
-    for i in range(0, 5-maxindex):
-        r = random.random()
-        if r < PROB_MINE:
-            newline = newline + "1"
-        else:
-            newline = newline + "0"
-        r = random.random()
-        if r < PROB_BRIDGE:
-            bridgeline = bridgeline + "1"
-        else:
-            bridgeline = bridgeline + "0"
-    lastindex = newindex
-    output.write(bridgeline + "\t")
-    output.write(newline + "\n")
+for song in SONGS:
+    input = open("../../../data/" + song + "-times.txt", "r")
+    output = open("../../../data/" + song + "-level.txt", "w")
+    linenumber = 0
+    lastindex = START_STRING
     
+    for line in input:
+        linenumber = linenumber + 1
+        line = line.rstrip()
+        if linenumber == 1:
+            output.write(str(0.0) + "\t")
+        else:
+            output.write(line + "\t")
+        if linenumber % MOVE_FREQ == 1:
+            r = random.random()
+            if r < PROB_DOWN:
+                move = -1
+            elif r < 1-PROB_UP:
+                move = 0
+            else:
+                move = 1
+            if (lastindex == 0 and move == -1) or (lastindex == 5 and move == 1) or (move == 0):
+                newindex = lastindex
+            else:
+                newindex = lastindex + move     
+        else:
+            newindex = lastindex
+        bridgeline = ""
+        newline = ""
+        minindex = min(lastindex, newindex)
+        maxindex = max(lastindex, newindex)
+        for i in range(0, minindex):
+            r = random.random()
+            if r < PROB_MINE:
+                newline = newline + "1"
+            elif r < 1-PROB_POWERUP:
+                newline = newline + "0"
+            else:
+                newline = newline + "5"
+            r = random.random()
+            if r < PROB_BRIDGE:
+                bridgeline = bridgeline + "1"
+            else:
+                bridgeline = bridgeline + "0"
+        r = random.random()
+        if newindex == lastindex:
+            if r < PROB_ENEMY:
+                r = random.random()
+                cum = 0
+                for type, prob in PROB_ENEMY_TYPE:
+                    if cum <= r and r < cum + prob:
+                        newline = newline + str(type)
+                    cum = cum + prob
+            else:
+                newline = newline + "0"
+        else:
+            newline = newline + "00"
+            bridgeline = bridgeline + "1"
+        for i in range(0, 5-maxindex):
+            r = random.random()
+            if r < PROB_MINE:
+                newline = newline + "1"
+            elif r < 1-PROB_POWERUP:
+                newline = newline + "0"
+            else:
+                newline = newline + "5"
+            r = random.random()
+            if r < PROB_BRIDGE:
+                bridgeline = bridgeline + "1"
+            else:
+                bridgeline = bridgeline + "0"
+        lastindex = newindex
+        output.write(bridgeline + "\t")
+        output.write(newline + "\n")
+    input.close()
+    output.close()
